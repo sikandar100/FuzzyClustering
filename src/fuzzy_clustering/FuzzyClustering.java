1b2e460e70210982da594638a97f1e4362a9dfb2
Sikandar Waheed <ch.sikandar100@gmail.com>
2020-02-29 19:22:20 +0500

    Initial Commit
    This is an implimentation of FuzzyClustering in Java

---
+++ src/fuzzy_clustering/FuzzyClustering.java
@@ -0,0 +1,261 @@
+package fuzzy_clustering;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class FuzzyClustering {
+    public ArrayList<ArrayList<Float>> data;
+    public ArrayList<ArrayList<Float>> clusterCenters;
+    public ArrayList<ArrayList<String>> clusters;
+    private float u[][];
+    private float u_pre[][];
+    ArrayList<ArrayList<Integer>> centroids = new ArrayList<>();
+    private int clusterCount;
+    private int iteration;
+    private int dimension;
+    private int fuzziness;
+    private double epsilon;
+    public double finalError;
+
+    public FuzzyClustering(){
+        data = new ArrayList<>();
+        clusterCenters = new ArrayList<>();
+        fuzziness = 2;
+        epsilon = 0.01;
+    }
+    
+    public void run(int clusterNumber, int iter, ArrayList<ArrayList<Float>> data){
+        this.clusterCount = clusterNumber;
+        this.iteration = iter;
+        this.data = data;
+
+        //start algorithm
+        //1 assign initial membership values
+        assignInitialMembership();
+        int i;
+        for (i = 0; i < iteration; i++) {
+            //2 calculate cluster centers
+            calculateClusterCenters();
+
+            //3
+            updateMembershipValues();
+
+            //4
+            finalError = checkConvergence();
+            if(finalError <= epsilon)
+                break;
+        }
+        System.out.print("Covergence happens after "+ i+" iteratios: \n\n");
+    }
+
+    /**
+     * in this function we generate random data with specific option
+     * @param numberOfData
+     * @param dimension
+     * @param minRange
+     * @param maxRange
+     */
+    public void createRandomData(int numberOfData, int dimension, int minRange, int maxRange, int clusterCount){
+        this.dimension = dimension;
+        ArrayList<ArrayList<Integer>> centroids = new ArrayList<>();
+        centroids.add(new ArrayList<Integer>());
+        int[] numberOfDataInEachArea = new int[clusterCount];
+        int range = maxRange - minRange + 1;
+        int step = range / (clusterCount + 1);
+        for (int i = 1; i <= clusterCount; i++) {
+            centroids.get(0).add(minRange + i * step);
+        }
+
+        for (int i = 0; i < dimension - 1; i++) {
+            centroids.add((ArrayList<Integer>) centroids.get(0).clone());
+        }
+        double variance = (centroids.get(0).get(1) - centroids.get(0).get(0))/ 2.5;
+        for (int i = 0; i < dimension; i++) {
+            Collections.shuffle(centroids.get(i));
+        }
+        Random r = new Random();
+        int sum = 0;
+        for (int i = 0; i < clusterCount; i++) {
+            int rg = r.nextInt(50) + 10;
+            numberOfDataInEachArea[i] = (rg);
+            sum += rg;
+        }
+        for (int i = 0; i < clusterCount; i++)
+            numberOfDataInEachArea[i] = (int)((((double)numberOfDataInEachArea[i]) / sum) * numberOfData);
+
+        Random fRandom = new Random();
+        for (int i = 0; i < clusterCount; i++) {
+            for (int j = 0; j < numberOfDataInEachArea[i]; j++) {
+                ArrayList<Float> tmp = new ArrayList<>();
+                for (int k = 0; k < dimension; k++) {
+                    tmp.add((float)(centroids.get(k).get(i) + fRandom.nextGaussian() * variance));
+                }
+                data.add(tmp);
+            }
+        }
+    }
+
+    /**
+     * this function generate membership value for each data
+     */
+    private void assignInitialMembership(){
+        u = new float[data.size()][clusterCount];
+        u_pre = new float[data.size()][clusterCount];
+        Random r = new Random();
+        for (int i = 0; i < data.size(); i++) {
+            float sum = 0;
+            for (int j = 0; j < clusterCount; j++) {
+                u[i][j] = r.nextFloat() * 10 + 1;
+                sum += u[i][j];
+            }
+            for (int j = 0; j < clusterCount; j++) {
+                u[i][j] = u[i][j] / sum;
+            }
+        }
+    }
+
+    /**
+     * in this function we calculate value of each cluster
+     */
+    private void calculateClusterCenters(){
+        clusterCenters.clear();
+        //ArrayList<Float> testing = new ArrayList<>();
+        for (int i = 0; i < clusterCount; i++) {
+            ArrayList<Float> tmp = new ArrayList<>();
+            for (int j = 0; j < dimension; j++) {
+                float cluster_ij;
+                float sum1 = 0;
+                float sum2 = 0;
+                for (int k = 0; k < data.size(); k++) {
+                    double tt = Math.pow(u[k][i], fuzziness);
+                    sum1 += tt * data.get(k).get(j);
+                    sum2 += tt;
+                }
+                cluster_ij = sum1/sum2;
+                tmp.add(cluster_ij);
+            }
+            clusterCenters.add(tmp);
+            
+        }
+        //System.out.print(testing);
+    }
+
+    /**
+     * in this function we will update membership value
+     */
+    private void updateMembershipValues(){
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < clusterCount; j++) {
+                u_pre[i][j] = u[i][j];
+                float sum = 0;
+                float upper = Distance(data.get(i), clusterCenters.get(j));
+                for (int k = 0; k < clusterCount; k++) {
+                    float lower = Distance(data.get(i), clusterCenters.get(k));
+                    sum += Math.pow((upper/lower), 2/(fuzziness -1));
+                }
+                u[i][j] = 1/sum;
+            }
+        }
+        
+    }
+
+    /**
+     * get norm 2 of two point
+     * @param p1
+     * @param p2
+     * @return
+     */
+    private float Distance(ArrayList<Float> p1, ArrayList<Float> p2){
+        float sum = 0;
+        for (int i = 0; i < p1.size(); i++) {
+            sum += Math.pow(p1.get(i) - p2.get(i), 2);
+        }
+        sum = (float) Math.sqrt(sum);
+        return sum;
+    }
+
+    /**
+     * we calculate norm 2 of ||U - U_pre||
+     * @return
+     */
+    private double checkConvergence(){
+        double sum = 0;
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < clusterCount; j++) {
+                sum += Math.pow(u[i][j] - u_pre[i][j], 2);
+            }
+        }
+        return Math.sqrt(sum);
+    }
+
+    /**
+     * write random generated data to file for visualizing
+     * @throws IOException
+     */
+    public void writeDataToFile(ArrayList<ArrayList<Float>> inpData, String fileName) throws IOException {
+
+        FileWriter fileWriter = new FileWriter("./" + fileName + ".csv");
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+
+        for (int i = 0; i < inpData.size(); i++) {
+             String res = "";
+            for (int j = 0; j < inpData.get(i).size(); j++) {
+                if(j == inpData.get(i).size() - 1)
+                    res += inpData.get(i).get(j);
+                else
+                    res += inpData.get(i).get(j) +",";
+            }
+            printWriter.println(res);
+        }
+        printWriter.close();
+    
+    }
+    
+    public void DisplayMatrixU() {
+    	
+    	//printing out the membership matrix
+        
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < clusterCount; j++) {
+                System.out.print(u[i][j]+"  ");
+                } 
+            System.out.println();
+            }
+    }
+    
+    public void displayClusters()
+    {
+    	
+    	
+        for (int i = 0; i < data.size(); i++) {
+        		int j=0;
+        		int idx=0;
+            	int temp = clusterCount;
+                while(temp>1) {
+                	if (Math.round(u[i][j])>Math.round(u[i][j+1]))
+                	{
+                		System.out.print(data.get(i)+" C"+(idx+1));
+                		
+                	}
+                	else if(Math.round(u[i][j])<Math.round(u[i][j+1]))
+                	{
+                		System.out.print(data.get(i)+" C"+((j+1)+1));
+                		
+                	}
+                	else if(Math.round(u[i][j])==Math.round(u[i][j+1]))
+                	{
+                		System.out.print(data.get(i)+" C"+(idx+1)+" & C"+((j+1)+1));
+                		
+                	}
+                	j++;
+                	temp--;
+                	System.out.println();
+                  }
+                } 
+    }
+
+    
+}
---
+++ src/fuzzy_clustering/Main.java
@@ -0,0 +1,34 @@
+package fuzzy_clustering;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        FuzzyClustering cmean = new FuzzyClustering();
+
+        //get number of class from user
+        System.out.println("Please input number of cluster that you want :");
+        Scanner sc= new Scanner(System.in);
+        String read1 = sc.nextLine();
+        System.out.println("please input size of data set :");
+        String read2 = sc.nextLine();
+
+        //generate random data
+        cmean.createRandomData(Integer.parseInt(read2),2,1,100, Integer.parseInt(read1));
+
+        //write random data
+        cmean.writeDataToFile(cmean.data, "data_set");
+
+        //run clustering algorithm
+        cmean.run(Integer.parseInt(read1), 100, cmean.data);
+
+        //write cluster center to file
+        cmean.writeDataToFile(cmean.clusterCenters, "cluster_centers");
+        cmean.DisplayMatrixU();
+        cmean.displayClusters();
+        System.out.println("Clustering Finished!!!");
+        
+    }
+}
